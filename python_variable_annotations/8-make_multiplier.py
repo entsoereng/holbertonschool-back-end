9ceb02b3fd727a33b603866c167eb17f289c19bc
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-04-23 14:18:02 +0200

    Create 8-make_multiplier.py

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,7 @@
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies a float by the given multiplier."""
+    def multiplier_function(x: float) -> float:
+        return x * multiplier
+    return multiplier_function
